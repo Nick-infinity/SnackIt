118c6e07e7fad962bd27fe7c83ac702fef0d3da2
Nickinfinity <nikhilg.me@gmail.com>
2021-01-19 16:26:01 +0530

    Adding Sliding Fragment Screens

--- app/src/main/java/com/nickinfinity/sizzlebite/IntroductoryActivity.java
+++ app/src/main/java/com/nickinfinity/sizzlebite/IntroductoryActivity.java
@@ -4,14 +4,24 @@ import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import com.airbnb.lottie.LottieAnimationView;
 
 public class IntroductoryActivity extends AppCompatActivity {
+
+    // Variables
     ImageView img_bg;
     TextView tv_name;
     LottieAnimationView lottieAnimationView;
+    private static final int NUM_PAGES = 2 ;
+    private ViewPager viewPager;
+    private ScreenSlidePageAdaper screenSlidePageAdaper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,11 +32,42 @@ public class IntroductoryActivity extends AppCompatActivity {
         img_bg = findViewById(R.id.img_bg);
         tv_name = findViewById(R.id.tv_name);
         lottieAnimationView = findViewById(R.id.lottie_splash);
+        viewPager = findViewById(R.id.pager);
+        screenSlidePageAdaper = new ScreenSlidePageAdaper(getSupportFragmentManager());
+        viewPager.setAdapter(screenSlidePageAdaper);
+
 
         // Animate
         img_bg.animate().translationY(-2900).setDuration(1000).setStartDelay(4000);
         tv_name.animate().translationY(-1400).setDuration(1000).setStartDelay(4000);
         lottieAnimationView.animate().translationY(1400).setDuration(1000).setStartDelay(4000);
 
+    }
+
+    private class ScreenSlidePageAdaper extends FragmentStatePagerAdapter {
+        public ScreenSlidePageAdaper(@NonNull FragmentManager fm) {
+            super(fm);
+        }
+
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            switch (position) {
+                case 0:
+                    OnboardFrag1 tab1 = new OnboardFrag1();
+                    return tab1;
+                case 1:
+                    OnboardFrag2 tab2 = new OnboardFrag2();
+                    return tab2;
+
+
+            }
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+            return NUM_PAGES;
+        }
     }
 }
---
+++ app/src/main/java/com/nickinfinity/sizzlebite/OnboardFrag2.java
@@ -0,0 +1,20 @@
+package com.nickinfinity.sizzlebite;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class OnboardFrag2 extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        ViewGroup root =(ViewGroup) inflater.inflate(R.layout.onboard_frag2,container,false);
+        return root;
+    }
+}
